90b4b914b2a4b5a5364b9742a94cbb2cc569a401
Kseniia <fedotova.ka2021@mail.ru>
2024-12-30 22:15:39 +0300

    Update tests for calculate method

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -2,12 +2,37 @@
 package com.example;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
+
     @Test
     public void testAdd() {
-      Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+        assertEquals(5, Calculator.calculate(2, 3, '+'), 0.001);
     }
+
+   @Test
+   public void testSubstract(){
+        assertEquals(-1, Calculator.calculate(2, 3, '-'), 0.001);
+   }
+
+   @Test
+   public void testMultiply(){
+        assertEquals(6, Calculator.calculate(2, 3, '*'), 0.001);
+   }
+
+   @Test
+   public void testDivide(){
+        assertEquals(2, Calculator.calculate(6, 3, '/'), 0.001);
+   }
+  
+   @Test(expected = IllegalArgumentException.class)
+   public void testInvalidOperator() {
+       Calculator.calculate(2, 3, '%');
+   }
+  
+   @Test(expected = ArithmeticException.class)
+   public void testDivideByZero() {
+       Calculator.calculate(2, 0, '/');
+   }
   }
